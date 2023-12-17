725e2ec943afbd16ade23d9f7315e2901448d3a6
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-12-17 20:23:16 +0530

    .

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { defineMiddleware } from "astro:middleware";
-
-export const onRequest = defineMiddleware(async (context, next) => {
-    const response = await next()
-    if (!context.request.url.includes('/api/upload')) {
-        response.headers.set('Cloudflare-CDN-Cache-Control', 'max-age=24400')
-    }
-    return response
-});
